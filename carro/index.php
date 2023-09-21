8f6adff50b74bfee7d63fd47f710bca96ce8c7c1
Raul Vithor Gibertoni Alvares Leal <115294229+Raul-Vithor@users.noreply.github.com>
2023-09-20 23:58:12 -0400

    Update and rename criaCarro.php to index.php

---
+++ carro/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <h2>cadastro de carros</h2>
+    <form action="logicaCarro.php" method="POST">
+        <input type="text" placeholder="Nome do carro" name="carroNome"/>
+        <input type="text" placeholder="Marca do carro" name="carroMarca"/>
+        <input type="text" placeholder="Modelo do carro" name="carroModelo"/>
+        <input type="text" placeholder="Ano do carro" name="carroAno"/>
+        <input type="submit" value="cadastrar"/>
+    </form>
+    </body>
+</html>
